203c5c970fb23321bde1f3efbf43ebd6dc0a69ae
Rho <rhodaphillipsosei@gmail.com>
2022-01-19 18:44:57 -0500

    create client file, export to play.js

---
+++ client.js
@@ -0,0 +1,17 @@
+// establishes a connection with the game server
+const net = require("net");
+const connect = function () {
+    const conn = net.createConnection({
+    host: '165.227.47.243', 
+    port: 50541
+    });
+    conn.on("connect", () => {
+    console.log("You are now connected!")
+    });
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+    
+    return conn;
+    };
+
+    module.exports = connect
--- play.js
+++ play.js
@@ -1,19 +1,8 @@
 const net = require("net");
+const connect = require("./client.js")
 
 // establishes a connection with the game server
-const connect = function () {
-const conn = net.createConnection({
-host: '165.227.47.243', 
-port: 50541
-});
-conn.on("connect", () => {
-console.log("You are now connected!")
-});
-// interpret incoming data as text
-conn.setEncoding("utf8");
 
-return conn;
-};
 
 connect();
 console.log("Connecting ...");
